92f9a1a8be344d7c56968bd56caf0bb830475163
佳琪曹 <353938360@qq.com>
2018-05-17 11:21:29 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 路由就是根据网址的不同，返回不同的页面
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import fasterclick from 'fastclick'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 Vue.config.productionTip = false
+fasterclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
+  router: router,
+  components: {App: App},
   template: '<App/>'
 })
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe697;</div>
+    </div>
+
+    <div class="header-input">
+      <span class="iconfont">&#xe6ac;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">城市
+      <span class="iconfont arrow-down">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home'
+  }
+</script>
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  .header
+    display: flex
+    line-height: .86rem
+    background-color: $bgColor
+    color #fff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
+        text-align center
+        font-size .4rem
+    .header-input
+      padding-left .2rem
+      flex: 1
+      line-height .64rem
+      margin-top .12rem
+      color #ccc
+      margin-bottom .12rem
+      background #fff
+      border-radius .1rem
+    .header-right
+      .arrow-down
+        margin-left -.04rem
+        font-size .1rem
+      width: 1.2rem
+      float: right
+      text-align center
+
+</style>
