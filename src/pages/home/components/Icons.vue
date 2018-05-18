8cf849424ac31f8a01ebf915a3ee8c4015cea4d0
佳琪曹 <353938360@qq.com>
2018-05-18 10:35:23 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,144 @@
+<template>
+    <div class="icons"> 
+      <swiper :options="swiperOption">
+        <swiper-slide v-for="(page, index) of pages" :key="index">
+            <div class="icon"
+            v-for="item of page" 
+            :key="item.id">
+                <div class="icon-img">
+                    <img class="icon-img-content" :src="item.imgUrl"/>
+                </div>
+                    <p class="icon-desc" v-text="item.desc"></p>
+                </div> 
+        </swiper-slide>
+      </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "HomeIcons",
+  data() {
+    return { 
+      swiperOption: {
+          loop:false
+      },
+      iconList: [
+        {
+          id: "001",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png",
+          desc: "自然风光加啊啊"
+        },
+        {
+          id: "002",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png",
+          desc: "滑雪季节"
+        },
+        {
+          id: "003",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png",
+          desc: "泡温泉"
+        },
+        {
+          id: "004",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png",
+          desc: "动物眼"
+        },
+        {
+          id: "006",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png",
+          desc: "景点门票"
+        },
+        {
+          id: "007",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png",
+          desc: "一日游"
+        },
+        {
+          id: "008",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png",
+          desc: "云南"
+        },
+        {
+          id: "009",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png",
+          desc: "昆明"
+        },
+        {
+          id: "010",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png",
+          desc: "大连"
+        }
+      ]
+    };
+  },
+  computed:{
+      pages (){
+        const pages=[]
+        this.iconList.forEach((item,index)=>{
+            const page = Math.floor(index/8)
+            if(!pages[page]){
+                pages[page] = []
+            }
+            pages[page].push(item)
+        })
+        return pages
+      }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+@import '~styles/mixins.styl';
+.icons >>> .swiper-container {
+    height: 0;
+    padding-bottom: 50%;
+
+    .icon {
+        position: relative;
+        overflow: hidden;
+        float: left;
+        width: 25%;
+        height: 0;
+        padding-bottom: 25%;
+
+        .icon-img {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0.44rem;
+            box-sizing: border-box;
+            padding: 0.1rem;
+
+            .icon-img-content {
+                display: block;
+                margin: 0 auto;
+                height: 100%;
+            }
+        }
+
+        .icon-desc {
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            height: 0.44rem;
+            line-height: 0.44rem;
+            color: $darkTextColor;
+            text-align: center;
+            ellipsis()
+        }
+    }
+}
+</style>
